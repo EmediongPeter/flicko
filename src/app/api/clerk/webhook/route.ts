44d5e4a604718b7828408b6f14f0e813945ff1e3
EmediongPeter <emedionguseh18@gmail.com>
2025-02-18 06:41:07 -0800

    push

--- src/app/api/clerk/webhook/route.ts
+++ src/app/api/clerk/webhook/route.ts
@@ -1,22 +1,23 @@
+import { NextRequest } from "next/server";
 import { db } from "~/server/db";
 
-export const POST = async (req: Request) => {
-    const { data } = await req.json();
-    
-    const emailAddress = data.email_addresses[0].email_address;
-    const firstName = data.first_name;
-    const lastName = data.last_name;
-    const imageUrl = data.image_url;
-    const id = data.id;
+export const POST = async (req: NextRequest) => {
+  const { data } = await req.json();
 
-    console.log(data)
-    await db.user.upsert({
-        where: { id },
-        update: { emailAddress, firstName, lastName, imageUrl },
-        create: { id, emailAddress, firstName, lastName, imageUrl },
-    });
+  const emailAddress = data.email_addresses[0].email_address;
+  const firstName = data.first_name;
+  const lastName = data.last_name;
+  const imageUrl = data.image_url;
+  const id = data.id;
 
-    console.log(await db.user.findMany())
+  const _ = await db.user.upsert({
+      where: { id },
+      update: { emailAddress, firstName, lastName, imageUrl },
+      create: { id, emailAddress, firstName, lastName, imageUrl },
+  })
+  console.log(await _)
 
-    return new Response('Webhook received', { status: 200 });
-}
+  // console.log(await db.user.findMany())
+
+  return new Response('Webhook received', { status: 200 });
+};
